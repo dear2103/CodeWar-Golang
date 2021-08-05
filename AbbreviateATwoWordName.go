68fc1838e28742b367b41bf7360871695af7166c
Hoang Yen <79043314+dear2103@users.noreply.github.com>
2021-08-05 19:56:33 +0700

    Update AbbreviateATwoWordName.go

--- AbbreviateATwoWordName.go
+++ AbbreviateATwoWordName.go
@@ -8,6 +8,7 @@ import (
 func AbbrevName(name string) string {
 	//your code here
 	s := strings.Split(name, " ")
+	hello
 	a1 := string(name[0])
 	a2 := string(name[len(name)-len(s[1])])
 	sf := fmt.Sprintf("%v.%v", strings.ToUpper(a1), strings.ToUpper(a2))
